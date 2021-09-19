1130e59ec0290234218afb3ad1c6c5c4e8f974ed
Cathleen Leone <cathleenl@Cathleens-MacBook-Pro.local>
2021-09-19 13:16:02 -0400

    added the psnr calculations for the zooming question

--- zooming.m
+++ zooming.m
@@ -6,9 +6,17 @@ low_res_I = imresize(img, 0.25, "bilinear");
 
 zoom_neighbour = imresize(low_res_I, 4, "nearest");
 zoom_bilinear = imresize(low_res_I, 4, "bilinear");
-zoom_cubic = imresize(low_res_I, 4, "cubic");
+zoom_cubic = imresize(low_res_I, 4, "bicubic");
 
 imshow(zoom_neighbour)
 %imshow(zoom_bilinear)
 %imshow(zoom_cubic)
 
+disp("Nearest neighbour psnr: ")
+psnr1 = psnr(img, zoom_neighbour)
+
+disp("Bilinear interpolation psnr: ")
+psnr2 = psnr(img, zoom_bilinear)
+
+disp("Bicubic interpolation psnr: ")
+psnr3 = psnr(img, zoom_cubic)
